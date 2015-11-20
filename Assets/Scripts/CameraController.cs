634ddf134a72557582fff12768c4be49a0bf668f
Trakton <higor.cmbotelho@gmail.com>
2015-11-20 21:34:50 -0200

    Não deixar ver fora do mapa
    Foram adicionado dois Scripts
    
    -CameraController, responsável por fazer a camera seguir o jogador
    suavemente.
    
    -CameraUnfollower, responsável por fazer com que a camera pare de seguir
    quando este entra em uma certa área. Este será utilizado por exemplo,
    quando o jogador atinge os limites do mapa, para que ele não possa ver
    além destes.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+    /*
+        Desenvolvido por: Higor
+        Este script tem o controle sobre o movimento da câmera, fazendo com que
+        ela siga o PC de forma suave. Caso 'followingPlayer' seja setado para falso,
+        a camera para de seguir o jogador. Isso geralmente acontecerá quando o
+        jogador chegar aos limites dos mapas, sendo necessário parar a câmera para
+        que ele não consiga ver fora destes limites.
+    */
+
+    public bool followingPlayerX;
+    public bool followingPlayerY;
+    public float followSpeed = 7.5f;
+    Transform player;
+	void Start ()
+    {
+        followingPlayerX = true;
+        followingPlayerY = true;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	void Update ()
+    {
+        Vector3 target = transform.position;
+        if(followingPlayerX) target.x = player.transform.position.x;
+        if(followingPlayerY) target.y = player.transform.position.y;
+        transform.position = Vector3.Lerp(transform.position, target, followSpeed * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/CameraUnfollower.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraUnfollower : MonoBehaviour
+{
+    /*
+        Desenvolvido por: Higor
+        Este script é responsável por impedir que a camera siga o jogador dando
+        'unfollowPlayer()' nela. Ele deve ser colocado em um GameObject junto com um
+        Collider2D, e é quando o jogador entrar dentro da área deste collider que o script
+        dará unfollow na camera. Isto ocorrerá frequentemente quando, por exemplo, o jogador
+        chegar aos limites do cenário, sendo necessário travar a câmera para que ele não
+        possa ver fora destes limites.
+    */
+
+    CameraController cameraController;
+    public bool unfollowX;
+    public bool unfollowY;
+
+	void Start ()
+    {
+        cameraController = Camera.main.GetComponent<CameraController>();
+	}
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if(col.tag == "Player")
+        {
+            if (unfollowX) cameraController.followingPlayerX = false;
+            if (unfollowY) cameraController.followingPlayerY = false;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            if (unfollowX) cameraController.followingPlayerX = true;
+            if (unfollowY) cameraController.followingPlayerY = true;
+        }
+    }
+}
