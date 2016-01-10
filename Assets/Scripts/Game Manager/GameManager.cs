b1f518eb58115169195521c20bc4b1b20930e60a
Trakton <higor.cmbotelho@gmail.com>
2016-01-10 00:37:24 -0200

    GameManager singleton prefab created
    The GameManger will be vital for controlling quests and the overall game
    progress.
    
    Two new scripts were created:
    - GameManager
    - Loader
    
    A GameManager prefab is ready, and the Camera got the Loader prefab
    attached to it.

---
+++ Assets/Scripts/Game Manager/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour 
+{
+	/*
+		Developed by: Higor
+
+		Description: The GameManager is a prefab responsible for storing information about
+		all the quests and overall game progress. To do it, we call DontDestroyOnLoad() for
+		keeping it alive across the scenes. However, there must never be more than one instance
+		of the GameManager on the game, that's why we make it be a singleton. A singleton is an object
+		that is instantiated once the game starts, and then never again.
+
+		How to use it: Attach it to the GameManger prefab. But you can't just drag the GameManager prefab on the scenes, because of it's
+		singleton behavior. You have to instantiate him on the first scenes, but using a script, the so called Loader.
+		*See the Loader script for more information
+		
+	 */
+
+	public static GameManager instance = null;
+
+	void Awake () 
+	{
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy (gameObject);
+		DontDestroyOnLoad (gameObject);
+	}
+
+	void Update () 
+	{
+	
+	}
+}
---
+++ Assets/Scripts/Game Manager/SceneChanger.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneChanger : MonoBehaviour 
+{
+
+	/*
+		Developed by: Higor
+
+		Description: This script is responsible for changing from one scene to another, making a FadeIn/FadeOut effect
+		in the process.
+
+		How to use it: Create a prefab with a single BoxCollider2D and attach the script to it. Fill the destinyScene variable
+		and that's it. When the player trigger it, it will change the scene.
+		
+	 */
+
+	public string destinyScene = "";
+
+	IEnumerator ChangeScene()
+	{
+		//To make the fade in, we simple call the ScreenFader script, present in the GameManager object, to start the fade.
+		//*See the ScreenFader script and the GameManger script for more information.
+		float fadeTime = GameObject.FindGameObjectWithTag ("GameManager").GetComponent<ScreenFader> ().BeginFade (1);
+		//Then wait until the fade is over
+		yield return new WaitForSeconds (fadeTime);
+		//And finally change the scene
+		Application.LoadLevel (destinyScene);
+	}
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		StartCoroutine (ChangeScene());
+	}
+}
