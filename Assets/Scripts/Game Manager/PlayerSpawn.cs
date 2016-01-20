e4d97d694ed5348515874dbb56bf9343d54d36bc
Trakton <higor.cmbotelho@gmail.com>
2016-01-20 15:14:47 -0200

    SceneChanger and PlayerSpawn scripts replaced
    The PlayerSpawn was changed for a really better logic and functioning.
    It works globally now, for any room, with less trouble.

--- Assets/Scripts/Game Manager/PlayerSpawn.cs
+++ Assets/Scripts/Game Manager/PlayerSpawn.cs
@@ -10,37 +10,36 @@ public class PlayerSpawn : MonoBehaviour
 		Description: This script is responsible for keeping track of the player positions when it is changing between scenes.
 		For example: you are in the Cin, and the you enter a teacher room. The game scene will change from Cin to Room.
 		When you leave the room, you obviously wanna get back in front of the Room door in the Cin. But this is not
-		going to happen, because the player will be teleported for the position he is originally set up in the scene. So, when the player
-		enters the room, we must save the room door position and when it leaves, manually move the player for that position. That's what
-		this script does.
+		going to happen, because the player will be teleported for the position he is originally set up in the scene. So,
+        when changing scenes, you save the name of the scene you are leaving. Then, when you enter the new scene, it searchs
+        for any "SceneChanger" object that is named like your leavingScene, get it's children object named "Spawn", wich must
+        be positioned on the position you want the player to spawn and teleport the player to it.
 
 		How to use it: It must be attached to the GameManager prefab.
 	
 	 */
 
-	static Vector3 target;
-	static string level;
+	static string leavingScene;
 
 	/// <summary>
-	/// Sets the current position the player must appear when he get's back for this scene
+	/// Sets the current level the player is beforing moving for another scene
 	/// </summary>
-	/// <param name="currentPosition">The position the player is before leaving the scene</param>
 	/// <param name="currentLevel">The scene he is leaving</param>
-	public static void SetTarget(Vector3 currentPosition, string currentLevel)
+	public static void SetTarget(string currentLevel)
 	{
-		target = currentPosition;
-		level = currentLevel;
+        leavingScene = currentLevel;
 	}
 
 	void OnLevelWasLoaded()
 	{
-		if (Application.loadedLevelName == level && target != Vector3.zero) 
-		{
-			Transform player = GameObject.FindGameObjectWithTag ("Player").transform;
-			player.position = new Vector3 (target.x, target.y, player.position.z);
-			Camera.main.transform.position = new Vector3 (target.x, target.y, Camera.main.transform.position.z);
-			target = Vector3.zero;
-			level = null;
-		}
+        Transform target = GameObject.Find(leavingScene).GetComponent<Transform>();
+
+        if(target != null)
+        {
+            Transform spawn = target.GetChild(0).GetComponent<Transform>();
+            Transform player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+            Vector3 position = new Vector3(spawn.position.x, spawn.position.y, player.position.z);
+            player.position = position;
+        }
 	}
 }
--- Assets/Scripts/Game Manager/SceneChanger.cs
+++ Assets/Scripts/Game Manager/SceneChanger.cs
@@ -10,15 +10,17 @@ public class SceneChanger : MonoBehaviour
 		Description: This script is responsible for changing from one scene to another, making a FadeIn/FadeOut effect
 		in the process.
 
-		How to use it: Create a prefab with a single BoxCollider2D and attach the script to it. Fill the destinyScene variable
-		and that's it. When the player trigger it, it will change the scene. The savePosition variable is important. If you,
-		for example, enter in a room, and then leave it, you will probably want your player to be next to the door when it leaves
-		the room. For this, we shall change it's position for in front of the door. So, if you want that to happen, savePosition must be true.
+		How to use it: Create a prefab with a single BoxCollider2D and attach the script to it. Fill the "destinyScene" variable
+        name with the scene you want to go, and name the prefab with exactly the same name. This is important. Then, created a new empty
+        gameobject, name it "Spawn" and set it as children of the SceneChanger main prefab. Change the position of the "Spawn
+        for setting exaclty where the player will appear when he get's back for this scene. It's generally in front on the
+        doors. But be careful not to put it too close for the SceneChanger main prefab BoxCollider2D, because if it happens
+        the player will most likely trigger it as soon as he appears in the scene, and we don't want this! 
+        *Please see the PlayerSpawn script for more information.
 		
 	 */
 
 	public string destinyScene = "";
-	public bool savePosition = false;
 	public bool locked = false;
 
 	IEnumerator ChangeScene()
@@ -27,10 +29,9 @@ public class SceneChanger : MonoBehaviour
 		//*See the ScreenFader script and the GameManger script for more information.
 		float fadeTime = GameObject.FindGameObjectWithTag ("GameManager").GetComponent<ScreenFader> ().BeginFade (1);
 		//Then wait until the fade is over
-		yield return new WaitForSeconds (fadeTime);
+		yield return new WaitForSeconds (1);
 		//And finally change the scene
-		if(savePosition)
-			PlayerSpawn.SetTarget(this.transform.position, Application.loadedLevelName);
+		PlayerSpawn.SetTarget(Application.loadedLevelName);
 
 		Application.LoadLevel (destinyScene);
 	}
