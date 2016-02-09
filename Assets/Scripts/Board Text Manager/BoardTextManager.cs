c47f8056952b6d095499bd7a6073050d2daf0dd8
LcsSantana <lss5@cin.ufpe.br>
2016-02-09 02:30:33 -0300

    Board Script Creation
    A script named BoardTextManager was created.

---
+++ Assets/Scripts/Board Text Manager/BoardTextManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardTextManager : MonoBehaviour {
+
+    public GUIContent content;
+    public GUIStyle style;
+    private bool activated;
+
+	// Use this for initialization
+	void Start () {
+        activated = false;
+        
+	}
+	
+	// Update is called once per frame
+	void OnGUI () {
+        if(activated)
+        {
+            GUI.Box(new Rect(0, 0, Screen.width, Screen.height), content, style);
+        }
+	    
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.name == "Player")
+        {
+            activated = true;
+            print("hellooo");
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.name == "Player")
+        {
+            activated = false;
+        }
+    }
+}
