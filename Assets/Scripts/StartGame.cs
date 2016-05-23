7a8e28c79cd2fb5c24389ec55eed1f4fec6e21fc
pedrootorres <phtg@cin.ufpe.br>
2016-05-23 16:13:40 -0300

    XML being laoded on the built version

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -1,7 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 using UnityEngine;
-//using UnityEditor;
 using UnityEngine.UI;
 using System;
 
@@ -17,33 +16,12 @@ public class StartGame : MonoBehaviour {
     }
 
 	public void startGame() {
-		/*print ("passei aqui");
-		GameObject controlCanvas = GameObject.Find("InitialMenu Canvas");
-		print (controlCanvas.name);
-		controlCanvas.SetActive (true);
 		Dictionary<int, Quest> quests = GameManager.instance.questManager.getQuests ();
 		Quest x = null;
 		quests.TryGetValue (1, out x);
-		print (x.name);*/
-		sceneChanger.Change();
-	}
 
-	string GetInputButtonName(string name) {
-		/*var inputManager = AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/InputManager.asset")[0];
-		SerializedObject obj = new SerializedObject(inputManager);
-		SerializedProperty axisArray = obj.FindProperty("m_Axes");
-
-		string positiveButton = "";
-
-		for (int i = 0; i < axisArray.arraySize; i++) {
-			if (axisArray.GetArrayElementAtIndex (i).displayName == name) {
-				positiveButton = axisArray.GetArrayElementAtIndex (i).FindPropertyRelative ("positiveButton").stringValue;
-				return positiveButton.ToUpper();
-			}
-		}
-
-		return positiveButton;*/
-		return "boramim";
+		GameObject.Find ("Start").transform.GetChild(0).GetComponent<Text> ().text = x.name;
+//		sceneChanger.Change();
 	}
 
 	public void quitGame() {
