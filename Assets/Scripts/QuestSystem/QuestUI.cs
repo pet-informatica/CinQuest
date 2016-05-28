987d48bbf05777a0affa1a671fef9d08999b5036
Trakton <hcmb@cin.ufpe.br>
2016-05-27 23:34:56 -0300

    QuestManager UI started. Open and Close window methods ready.

---
+++ Assets/Scripts/QuestSystem/QuestUI.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class QuestUI : MonoBehaviour {
+
+    public GameObject canvas;
+    bool opened;
+	
+	void Start ()
+    {
+        CloseWindow();
+	}
+
+    public void OpenWindow()
+    {
+        Cursor.visible = true;
+        canvas.SetActive(true);
+        opened = true;
+    }
+
+    public void CloseWindow()
+    {
+        //Cursor.visible = false;
+        canvas.SetActive(false);
+        opened = false;
+    }
+	
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (opened) CloseWindow();
+            else OpenWindow();
+        }
+	}
+}
