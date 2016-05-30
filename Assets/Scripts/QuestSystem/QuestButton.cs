b294b6470619487b9e1200e89de62ee5a29e798f
Trakton <hcmb@cin.ufpe.br>
2016-05-30 17:56:25 -0300

    Quest UI almost done. Missing QuestInfo description scroll viewer.

---
+++ Assets/Scripts/QuestSystem/QuestButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class QuestButton : MonoBehaviour {
+
+    public Quest quest;
+
+    QuestUI questUI;
+
+    void Start()
+    {
+        questUI = GameObject.Find("QuestManager").GetComponent<QuestUI>();
+        GetComponentInChildren<Text>().text = quest.name;
+    }   
+
+    public void OnClick()
+    {
+        questUI.ChangeSelectedQuest(quest);
+    }
+}
--- Assets/Scripts/QuestSystem/QuestUI.cs
+++ Assets/Scripts/QuestSystem/QuestUI.cs
@@ -1,28 +1,85 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 using System.Collections;
+using UnityEngine.UI;
 
 public class QuestUI : MonoBehaviour {
 
-    public GameObject canvas;
+    public GameObject questBox;
+    public GameObject questInfo;
+    public GameObject questBoxContent;
+    public GameObject questButton;
+    public Text questInfoName;
+    RectTransform questBoxTransform;
     bool opened;
+    float questButtonHeight;
+
+    List<Quest> quests = new List<Quest>();
+    List<GameObject> buttons = new List<GameObject>();
 	
 	void Start ()
     {
+        questBoxTransform = questBoxContent.GetComponent<RectTransform>();
+        questButtonHeight = questButton.GetComponent<RectTransform>().rect.height;
+
+        LoadFakeQuests();
+        UpdateQuestBoxContent();
+
         CloseWindow();
 	}
 
+    /// <summary>
+    /// Temporary method only for testing purposes. Must be erased latter.
+    /// </summary>
+    void LoadFakeQuests()
+    {
+        for(int i = 0; i < 25; ++i)
+        {
+            Quest quest = new Quest(0, "Roubar comida " + i, "Testando a quest " + i, true, null, null, null);
+            quests.Add(quest);
+        }
+    }
+
+    /// <summary>
+    /// Remove all the buttons from QuestManager Box and insert new ones according to the current quests inside quest list.
+    /// </summary>
+    void UpdateQuestBoxContent()
+    {
+        foreach (GameObject obj in buttons) Destroy(obj);
+        buttons.Clear();
+
+        int buttonAmount = quests.Count;
+        questBoxTransform.sizeDelta = new Vector2(0, buttonAmount * questButtonHeight);
+        for(int i = 0; i < buttonAmount; ++i)
+        {
+            GameObject newButton = Instantiate(questButton) as GameObject;
+            buttons.Add(newButton);
+            RectTransform newButtonTransform = newButton.GetComponent<RectTransform>();
+            newButton.transform.SetParent(questBoxContent.transform, false);
+            newButtonTransform.anchoredPosition = new Vector2(0, -i * questButtonHeight);
+           
+            QuestButton buttonScript = newButton.GetComponent<QuestButton>();
+            buttonScript.quest = quests[i];
+        }
+
+    }
+
+    /// <summary>
+    /// Open the Quest Manager Canvas, setting it's gameobject to active
+    /// </summary>
     public void OpenWindow()
     {
-        Cursor.visible = true;
-        canvas.SetActive(true);
+        questBox.SetActive(true);
         opened = true;
     }
 
+    /// <summary>
+    /// Close the Quest Manager Canvas, setting it's gameobject to inactive
+    /// </summary>
     public void CloseWindow()
     {
-        //Cursor.visible = false;
-        canvas.SetActive(false);
+        questBox.SetActive(false);
+        questInfo.SetActive(false);
         opened = false;
     }
 	
@@ -34,4 +91,14 @@ public class QuestUI : MonoBehaviour {
             else OpenWindow();
         }
 	}
+
+    /// <summary>
+    /// Change the information in the QuestInfo box for matching a quest
+    /// </summary>
+    /// <param name="quest">The quest for displaying the info</param>
+    public void ChangeSelectedQuest(Quest quest)
+    {
+        questInfoName.text = quest.name;
+        questInfo.SetActive(true);
+    }
 }
