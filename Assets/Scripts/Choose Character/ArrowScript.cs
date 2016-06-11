a8fd89c4763bba895372837a9baf33f385d95e27
pedrootorres <phtg@cin.ufpe.br>
2016-06-11 17:43:13 -0300

    New layout with arrows

---
+++ Assets/Scripts/Choose Character/ArrowScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowScript : MonoBehaviour {
+
+	public bool leftArrow;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void nextCharacters() {
+		if (leftArrow) {
+			print ("pra esquerda");
+		} else {
+			print ("pra direita");
+		}
+	}
+}
