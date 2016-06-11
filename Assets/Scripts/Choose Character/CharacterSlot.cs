a70cbd8a3aa949f13f279afb4070f86730d6866b
pedrootorres <phtg@cin.ufpe.br>
2016-06-11 17:25:06 -0300

    First choose character menu

---
+++ Assets/Scripts/Choose Character/CharacterSlot.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class CharacterSlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnPointerEnter (PointerEventData eventData)
+	{
+		GameObject.Find ("Selected").transform.position = gameObject.transform.position;
+	}
+		
+	public void OnPointerExit (PointerEventData eventData)
+	{
+		
+	}
+}
