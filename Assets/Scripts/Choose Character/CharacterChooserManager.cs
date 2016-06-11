fd7d011ec0f53e25887afe04886719ee53976af0
pedrootorres <phtg@cin.ufpe.br>
2016-06-11 19:00:00 -0300

    Arrows working with lerp

--- Assets/Scripts/Choose Character/CharacterChooserManager.cs
+++ Assets/Scripts/Choose Character/CharacterChooserManager.cs
@@ -19,17 +19,41 @@ public class CharacterChooserManager : MonoBehaviour {
 
 	public void nextCharacters() {
 		if (currentOptionPanel + 1 < optionsPanels.Length) {
-			optionsPanels [currentOptionPanel].GetComponent<RectTransform> ().localPosition = new Vector3 (-1065, 0, 0);
+//			optionsPanels [currentOptionPanel].GetComponent<RectTransform> ().localPosition = new Vector3 (-1065, 0, 0);
+
+			StartCoroutine(MoveFunction(optionsPanels [currentOptionPanel], new Vector3(-1065, 0, 0)));
 			currentOptionPanel++;
-			optionsPanels [currentOptionPanel].GetComponent<RectTransform> ().localPosition = new Vector3 (0, 0, 0);
+
+//			optionsPanels [currentOptionPanel].GetComponent<RectTransform> ().localPosition = new Vector3 (0, 0, 0);
+			StartCoroutine(MoveFunction(optionsPanels [currentOptionPanel], new Vector3(0, 0, 0)));
 		}
 	}
 
 	public void previousCharacters() {
 		if (currentOptionPanel - 1 >= 0) {
-			optionsPanels [currentOptionPanel].GetComponent<RectTransform> ().localPosition = new Vector3 (1065, 0, 0);
+//			optionsPanels [currentOptionPanel].GetComponent<RectTransform> ().localPosition = new Vector3 (1065, 0, 0);
+			StartCoroutine(MoveFunction(optionsPanels [currentOptionPanel], new Vector3(1065, 0, 0)));
 			currentOptionPanel--;
-			optionsPanels [currentOptionPanel].GetComponent<RectTransform> ().localPosition = new Vector3 (0, 0, 0);
+//			optionsPanels [currentOptionPanel].GetComponent<RectTransform> ().localPosition = new Vector3 (0, 0, 0);
+			StartCoroutine(MoveFunction(optionsPanels [currentOptionPanel], new Vector3(0, 0, 0)));
+		}
+	}
+
+	IEnumerator MoveFunction(GameObject panel, Vector3 newPosition)
+	{
+		float timeSinceStarted = 0f;
+		while (true)
+		{
+			timeSinceStarted += Time.deltaTime * 2.0f;
+			panel.GetComponent<RectTransform> ().localPosition = 
+				Vector3.Lerp(panel.GetComponent<RectTransform> ().localPosition, newPosition, timeSinceStarted);
+
+			if (panel.GetComponent<RectTransform> ().localPosition == newPosition)
+			{
+				yield break;
+			}
+				
+			yield return null;
 		}
 	}
 }
