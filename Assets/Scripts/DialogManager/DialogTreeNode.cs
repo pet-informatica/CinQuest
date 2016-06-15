3098f67d79777b964b24405cc74caf7c7ad7deee
Trakton <hcmb@cin.ufpe.br>
2016-06-15 18:19:12 -0300

    Some scene assets set up for testing the new node.

--- Assets/Scripts/DialogManager/DialogTreeNode.cs
+++ Assets/Scripts/DialogManager/DialogTreeNode.cs
@@ -4,15 +4,117 @@ using UnityEngine;
 
 /// <summary>
 /// Developed by: Higor (hcmb)
-/// The node of a DialogTree. Contains a message to speak, a response, that is,
-/// the response from the player that make us to reach this node, and a list of
-/// children nodes to keep tree going.
+/// A DialogTreeNode is used for building a DialogTree. Shows text conversations only. Can be extended by other
+/// classes such as QuestTreeNode or RewardTreeNode in order to add additional logic to the process of a conversation,
+/// such as changing the state of a quest or giving a reward for the player.
 /// </summary>
 [CreateAssetMenu]
 [Serializable]
 public class DialogTreeNode : ScriptableObject
 {
-    public string response;
-    public string message;
-    public List<DialogTreeNode> children;
+    [SerializeField]
+    string response;
+    /// <summary>
+    /// The response from the player wich will make him reach this tree node.
+    /// </summary>
+    public string Response
+    {
+        get { return response; }
+    }
+
+    [SerializeField]
+    string message;
+    /// <summary>
+    /// The message that is displayed by this node in the GUI.
+    /// </summary>
+    public string Message
+    {
+        get { return message; }
+    }
+
+    [SerializeField]
+    List<DialogTreeNode> children;
+    /// <summary>
+    /// A list of every reachable TreeNode node from the current one.
+    /// </summary>
+    public List<DialogTreeNode> Children
+    {
+        get { return children; }
+    }
+
+    /// <summary>
+    /// Set to TRUE if there isn't any tree node reachable from the current one. Indicates the end of a dialog.
+    /// </summary>
+    public bool IsLeaf
+    {
+        get { return Children.Count == 0; }
+    }
+
+    /// <summary>
+    /// The number of reachable and avaiable nodes from the current one.
+    /// </summary>
+    public int AvaiableChildren
+    {
+        get
+        {
+            int avaiable = 0;
+            foreach(DialogTreeNode node in children)
+            {
+                if (node.IsAvaiable())
+                {
+                    avaiable++;
+                }
+            }
+            return avaiable;
+        }
+    }
+
+    /// <summary>
+    /// Constructs a new TreeNode
+    /// </summary>
+    /// <param name="response">The response avaiable for the player in GUI that will make him reach this node.</param>
+    /// <param name="message">The message displayed on GUI when this node is reached.</param>
+    /// <param name="children">A list of TreeNodes reachable from this one.</param>
+    public DialogTreeNode(string response, string message, List<DialogTreeNode> children)
+    {
+        this.response = response;
+        this.message = message;
+        this.children = children;
+    }
+
+    /// <summary>
+    /// Returns if it is possible to reach this node. The node can be BLOCKED by several reasons,
+    /// such as the player not having the required PreConditions. In a DialogTreeNode, every node
+    /// is reachable, but in inherited classes such as ConditionalTreeNode this method will check
+    /// if the players has all the preconditions specified in a list.
+    /// </summary>
+    /// <returns>TRUE if the current node can be reached. False otherwise.</returns>
+    public virtual bool IsAvaiable()
+    {
+        return true;
+    }
+
+    /// <summary>
+    /// Execute it when needed to traverse down the tree and go for a child node. Additional logic
+    /// can be added by inherited classes overriding this method in order to execute an action in 
+    /// the process of changing the current dialog tree node.
+    /// </summary>
+    /// <param name="response">The player response index for traversing the tree. If the index is
+    /// pointing to an UnnavaiableNode, the method will go for the first AvaiableNode it finds. If
+    /// there is node, it will return null.</param>
+    /// <returns>The child wich was reached by the player response index.</returns>
+    public virtual DialogTreeNode GoToChild(int response)
+    {
+        int index = -1;
+        while (response >= 0)
+        {
+            index++;
+            if (index >= Children.Count)
+                return null;
+
+            if (Children[index].IsAvaiable())
+                response--;
+        }
+        return Children[index];
+    }
 }
