3a6a4a7deacdf6fd2dca46ae0736fa34b3a790f3
Trakton <hcmb@cin.ufpe.br>
2016-06-26 11:56:46 -0300

    Minimap naming system done.
    There is now a class called SceneNamer, wich will be responsible from
    parsing formal scene names to agradable names for player to see in UI.

---
+++ Assets/Scripts/GameManager/SceneNamer.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Developed by: Higor (hcmb)
+/// Contains dictionaries for parsing scene names from unity format to UI displayable
+/// </summary>
+public class SceneNamer : MonoBehaviour
+{
+
+    static Dictionary<string, string> ui = new Dictionary<string, string>();
+
+    public void Start()
+    {
+        /* Menu */
+        ui.Add("GameOpening", "Main Menu");
+        ui.Add("ChooseCharacter", "Escolha de Personagem");
+        /* Cin */
+        ui.Add("Cin", "CIn");
+        ui.Add("TeacherRoomBig_1", "Sala de Professor");
+        ui.Add("TeacherRoomBig_2", "Sala de Professor");
+        ui.Add("TeacherRoomBig_3", "Sala de Professor");
+        ui.Add("TeacherRoomBig_4", "Sala de Professor");
+        ui.Add("TeacherRoomBig_5", "Sala de Professor");
+        ui.Add("TeacherRoomBig_6", "Sala de Professor");
+        ui.Add("TeacherRoomBig_7", "Sala de Professor");
+        ui.Add("TeacherRoomBig_8", "Sala de Professor");
+        ui.Add("TeacherRoomBig_9", "Sala de Professor");
+        ui.Add("TeacherRoomBig_10", "Sala de Professor");
+        ui.Add("TeacherRoomBig_11", "Sala de Professor");
+        ui.Add("TeacherRoomSmall_1", "Sala de Professor");
+        ui.Add("TeacherRoomSmall_2", "Sala de Professor");
+        ui.Add("TeacherRoomSmall_3", "Sala de Professor");
+        ui.Add("TeacherRoomSmall_4", "Sala de Professor");
+        ui.Add("TeacherRoomSmall_5", "Sala de Professor");
+        ui.Add("TeacherRoomSmall_6", "Sala de Professor");
+        ui.Add("TeacherRoomSmall_7", "Sala de Professor");
+        ui.Add("TeacherRoomSmall_8", "Sala de Professor");
+        ui.Add("TeacherRoomSmall_9", "Sala de Professor");
+        ui.Add("TeacherRoomSmall_10", "Sala de Professor");
+        ui.Add("TeacherRoomSmall_11", "Sala de Professor");
+        ui.Add("TeacherRoomSmall_12", "Sala de Professor");
+        ui.Add("Grad_1", "Grad 1");
+        ui.Add("Grad_2", "Grad 2");
+        ui.Add("Grad_3", "Grad 3");
+        ui.Add("Grad_4", "Grad 4");
+        ui.Add("Grad_5", "Grad 5");
+        ui.Add("ResearchLab_1", "Laboratório de Pesquisa");
+        ui.Add("ResearchLab_2", "Laboratório de Pesquisa");
+        ui.Add("BathroomF_1", "Banheiro Feminino");
+        ui.Add("BathroomF_2", "Banheiro Feminino");
+        ui.Add("BathroomM_1", "Banheiro Masculino");
+        ui.Add("BathroomM_2", "Banheiro Masculino");
+        ui.Add("Clasroom_1", "Sala de Aula");
+        ui.Add("Clasroom_2", "Sala de Aula");
+        ui.Add("LivingRoom", "Sala de Convivência");
+        ui.Add("Anfitheater", "Anfiteatro");
+        ui.Add("Pet", "PET");
+        ui.Add("Citi", "CITI");
+        ui.Add("Voxar", "VOXAR");
+        ui.Add("Kitchen", "Copa");
+        /* CinParking */
+        ui.Add("CinParking", "Estacionamento");
+        /* CCEN */
+        ui.Add("CCEN", "CCEN");
+    }
+
+    /// <summary>
+    /// Converts a formal scene name to an agradable name for displaying for player in UI components
+    /// </summary>
+    /// <param name="scene">The formal scene name for converting</param>
+    /// <returns></returns>
+    public static string UI(string scene)
+    {
+        if (ui.ContainsKey(scene))
+            return ui[scene];
+        return "";
+    }
+}
---
+++ Assets/Scripts/Minimap/Minimap.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Minimap : MonoBehaviour {
+
+    Text title;
+
+	void Start () {
+        title = GetComponentInChildren<Text>();
+        title.text = SceneNamer.UI(SceneManager.GetActiveScene().name);
+    }
+
+    void OnLevelWasLoaded(int level)
+    {
+        if (SceneNamer.UI(SceneManager.GetActiveScene().name) != null)
+            if(title != null)
+                title.text = SceneNamer.UI(SceneManager.GetActiveScene().name);
+    }
+}
