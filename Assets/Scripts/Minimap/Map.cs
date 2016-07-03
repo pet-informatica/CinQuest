1738b0a262f34d9796ca592fa51c03f14160b1c3
Trakton <hcmb@cin.ufpe.br>
2016-07-03 19:47:07 -0300

    Map Canvas UI fully interative.
    Next steps will be including Drag/Zoom feature.

--- Assets/Scripts/Minimap/Map.cs
+++ Assets/Scripts/Minimap/Map.cs
@@ -1,15 +1,91 @@
-﻿using UnityEngine;
+﻿using System;
 using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
 
-public class Map : MonoBehaviour {
+/// <summary>
+/// The Global Map instanced classe
+/// Developed by: Higor (hcmb)
+/// </summary>
+public class Map : MonoBehaviour
+{
+    public AudioClip openingSound;
+    public GameObject mapObject;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    /// <summary>
+    /// Returns true if the global map is alredy opened.
+    /// </summary>
+    public bool IsOpen
+    {
+        get; private set;
+    }
+
+    private static Map instance = null;
+
+    /// <summary>
+    /// Gets the unique instance of the Map for static access
+    /// </summary>
+    public static Map Instance
+    {
+        get { return instance; }
+    }
+
+    /// <summary>
+    /// Awake this instance.
+    /// </summary>
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            IsOpen = true;
+            Close();
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Map"))
+        {
+            if (IsOpen) Close();
+            else Open();
+        }
+    }
+
+    /// <summary>
+    /// Opens the global map UI panel.
+    /// </summary>
+    public void Open()
+    {
+        if (checkIfMapIsOnScene() && !IsOpen)
+        {
+            IsOpen = true;
+            mapObject.SetActive(true);
+            Minimap.Instance.Close();
+        }
+    }
+
+    /// <summary>
+    /// Closes the map UI panel.
+    /// </summary>
+    public void Close()
+    {
+        if (checkIfMapIsOnScene() && IsOpen)
+        {
+            IsOpen = false;
+            mapObject.SetActive(false);
+            Minimap.Instance.Open();
+        }
+    }
+
+    /// <summary>
+    /// Checks if alert box is on scene.
+    /// </summary>
+    /// <returns><c>true</c>, if if alert is on scene, <c>false</c> otherwise.</returns>
+    private bool checkIfMapIsOnScene()
+    {
+        if (mapObject == null)
+            throw new NullReferenceException();
+        return true;
+    }
 }
