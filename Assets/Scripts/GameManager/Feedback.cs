bb72ee2dcdd077cae3dbbed34fdfa762427b19d8
pedrootorres <phtg@cin.ufpe.br>
2016-10-18 01:05:07 -0200

    Improvements on Feedback and Pause menus

--- Assets/Scripts/GameManager/Feedback.cs
+++ Assets/Scripts/GameManager/Feedback.cs
@@ -4,33 +4,14 @@ using System.Collections;
 
 public class Feedback : MonoBehaviour {
 
-	private GameObject feedbackCanvas;
-	private Feedback feedback;
 	public InputField emailFeed;
 	public InputField bodyFeed;
 
-	void Start () {
-		feedbackCanvas = GameObject.Find("Feedback Canvas");
-		feedback = feedbackCanvas.GetComponent<Feedback> ();
-	}
-
-	void Update () {
-	}
-
 	public void closeFeedback() {
-		feedback.cancel();
+		PauseMenu.Instance.CloseFeedback();
 	}
 
-	private void cancel(){
-		feedbackCanvas.SetActive (false);
-	}
-
-	public void emailFeedback() {
-		feedback.sendEmail();
-	}
-
-	private void sendEmail(){
-		
+	public void sendEmail(){
 		//email Id to send the mail to
 		string email = emailFeed.text;
 		//subject of the mail
