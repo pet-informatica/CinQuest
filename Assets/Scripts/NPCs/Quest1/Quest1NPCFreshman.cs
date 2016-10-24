c49274218c5913fea3943d91e2004a4bca3c2be2
Higor Cavalcanti Machado Botelho <hcmb@cin.ufpe.br>
2016-10-24 02:40:52 -0200

    Including missing files in commit

---
+++ Assets/Scripts/NPCs/Quest1/Quest1NPCFreshman.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Quest1NPCFreshman : MonoBehaviour, IBroadcaster {
+
+	public enum EState{
+		AtGate,
+		WaitingGatekeeper,
+		AtHelpdesk,
+		QuestEnd
+	}
+		
+	public Waypoint pathToHelpdesk;
+	InterativeSpeaker speaker;
+	Move move;
+	EState state;
+	int currentDialog;
+
+	void Start () {
+		state = EState.AtGate;
+		move = GetComponent<Move> ();
+		speaker = GetComponent<InterativeSpeaker> ();
+	}
+
+	void Update () {
+		
+	}
+
+	public void Broad(){
+		ChangeState ();
+	}
+
+	public void ChangeState(){
+		state++;
+		speaker.defaultDialogIndex++;
+	}
+}
