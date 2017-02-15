2d0e05f544d7cc8e073b46c2bf4e1556e7151b39
Higor Cavalcanti Machado Botelho <hcmb@cin.ufpe.br>
2017-02-15 14:16:47 -0200

    CCEN climb up stairs is now possible

---
+++ Assets/Scripts/NPCs/NPCMoverAuto.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Specializes the NPCMover class allowing the user to set a path for the player to
+/// automatically move through as soon as the script starts.
+/// </summary>
+public class NPCMoverAuto : NPCMover 
+{
+	public GameObject targetWaypoint;
+
+	void Start()
+	{
+		GoForTargetWaypoint (targetWaypoint);
+	}
+}
