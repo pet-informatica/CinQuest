218b9155ba66c49976468ddcc6dfba073cc681f9
Higor Cavalcanti Machado Botelho <hcmb@cin.ufpe.br>
2017-02-16 13:53:16 -0200

    Adding another stairs up scene to match 2 floor secgrad

--- Assets/Scripts/GameManager/SceneNamer.cs
+++ Assets/Scripts/GameManager/SceneNamer.cs
@@ -63,7 +63,7 @@ public class SceneNamer : MonoBehaviour
         ui.Add("CinParking", "Estacionamento");
         /* CCEN */
         ui.Add("CCEN", "CCEN");
-		ui.Add("CCENUpstairs", "CCEN - 1º Andar");
+		ui.Add("CCENUpstairs", "CCEN - 2º Andar");
 		ui.Add("SecGrad", "SecGrad");
     }
 
