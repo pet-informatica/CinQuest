37815842fc36e298be3faabd881a66d9d9d0828b
Higor Cavalcanti Machado Botelho <hcmb@cin.ufpe.br>
2017-02-16 00:19:42 -0200

    SecGrad npc set up and quest 1 can be fully ended

---
+++ Assets/Scripts/NPCs/Quest1/Quest1NPCSecGrad.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Quest1NPCSecGrad : MonoBehaviour, IBroadcaster {
+
+	enum State{
+		GivingLogin
+	};
+
+	State state;
+
+	public void Broad(){
+		if (state == State.GivingLogin) {
+			NPCListener.Instance.Disable ("Quest1CinParkingFreshmen");
+			NPCListener.Instance.Disable ("Quest1CCENFreshmen");
+			state++;
+		}
+	}
+}
