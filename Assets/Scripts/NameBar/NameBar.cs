e8069fd75d72129de2f4f2fa46645a63d78128bf
Rafael Nunes G. da Silveira <rngs@cin.ufpe.br>
2017-03-05 21:50:32 -0300

    NameBar added.

---
+++ Assets/Scripts/NameBar/NameBar.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class NameBar : MonoBehaviour {
+	public GameObject NameBarUI;
+	Text name;
+
+	public NameBar ()
+	{
+	}
+
+	private static NameBar instance = null;
+
+	void Awake()
+	{
+		if (instance == null)
+		{
+			instance = this;
+		}
+	}
+
+	void Start () {
+		name = GetComponentInChildren<Text>();
+		string playerName = GameManager.Instance.gameData.PlayerName;
+		if (playerName != null || !playerName.Equals ("")) {
+			name.text = playerName;
+		} else {
+			name.text = "No name!";
+		}
+	}
+}
